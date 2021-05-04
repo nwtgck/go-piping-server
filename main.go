bdc302f5abe9962fea0e3d63d4dbaa361ceb3732
Ryo Ota <nwtgck@nwtgck.org>
2021-05-04 17:27:42 +0900

    create s.getPipe() and use it

--- main.go
+++ main.go
@@ -27,25 +27,30 @@ func NewServer() *PipingServer {
 	}
 }
 
-func (s *PipingServer) Handler(resWriter http.ResponseWriter, req *http.Request) {
-	fmt.Println(req.Method)
-	path := req.URL.Path
-
+func (s *PipingServer) getPipe(path string) *pipe {
 	// Set pipe if not found on the path
 	s.mutex.Lock()
+	defer s.mutex.Unlock()
 	if _, ok := s.pathToPipe[path]; !ok {
-		s.pathToPipe[path] = &pipe{
+		pi := &pipe{
 			receiverResWriterCh: make(chan http.ResponseWriter, 1),
 			sendFinishedCh:      make(chan struct{}),
 			isSenderConnected:   0,
 		}
+		s.pathToPipe[path] = pi
+		return pi
 	}
-	s.mutex.Unlock()
-	pi := s.pathToPipe[path]
+	return s.pathToPipe[path]
+}
+
+func (s *PipingServer) Handler(resWriter http.ResponseWriter, req *http.Request) {
+	fmt.Println(req.Method)
+	path := req.URL.Path
 
 	// TODO: should close if either sender or receiver closes
 	switch req.Method {
 	case "GET":
+		pi := s.getPipe(path)
 		// If already get the path
 		if len(pi.receiverResWriterCh) != 0 {
 			resWriter.Header().Set("Access-Control-Allow-Origin", "*")
@@ -59,6 +64,7 @@ func (s *PipingServer) Handler(resWriter http.ResponseWriter, req *http.Request)
 	case "POST":
 		fallthrough
 	case "PUT":
+		pi := s.getPipe(path)
 		// If a sender is already connected
 		if !atomic.CompareAndSwapInt32(&pi.isSenderConnected, 0, 1) {
 			resWriter.Header().Set("Access-Control-Allow-Origin", "*")
