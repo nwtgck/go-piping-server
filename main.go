1f46218059b8a4564ffabc15a6fe3be7d87934c7
Ryo Ota <nwtgck@nwtgck.org>
2021-05-04 15:40:33 +0900

    respond error when multiple receiver on the same path

--- main.go
+++ main.go
@@ -7,48 +7,55 @@ import (
 	"net/http"
 )
 
-type Receiver struct {
-	responseWriterCh chan http.ResponseWriter
-	sendFinishedCh   chan struct{}
+type pipe struct {
+	receiverResWriterCh chan http.ResponseWriter
+	sendFinishedCh      chan struct{}
 }
 
 type PipingServer struct {
-	pathToReceiver map[string]*Receiver
+	pathToPipe map[string]*pipe
 }
 
 func NewServer() *PipingServer {
-	return &PipingServer{pathToReceiver: map[string]*Receiver{}}
+	return &PipingServer{pathToPipe: map[string]*pipe{}}
 }
 
-func (s *PipingServer) Handler(responseWriter http.ResponseWriter, req *http.Request) {
+func (s *PipingServer) Handler(resWriter http.ResponseWriter, req *http.Request) {
 	fmt.Println(req.Method)
 	path := req.URL.Path
 
-	// Set receiver if not found on the path
-	if _, ok := s.pathToReceiver[path]; !ok {
-		s.pathToReceiver[path] = &Receiver{
-			responseWriterCh: make(chan http.ResponseWriter),
-			sendFinishedCh:   make(chan struct{}),
+	// Set pipe if not found on the path
+	if _, ok := s.pathToPipe[path]; !ok {
+		s.pathToPipe[path] = &pipe{
+			receiverResWriterCh: make(chan http.ResponseWriter, 1),
+			sendFinishedCh:      make(chan struct{}),
 		}
 	}
-	receiver := s.pathToReceiver[path]
+	pi := s.pathToPipe[path]
 
-	// TODO: should block collision (e.g. GET the same path twice)
+	// TODO: should block collision (e.g. POST the same path twice)
 	// TODO: should close if either sender or receiver closes
 	switch req.Method {
 	case "GET":
-		go func() { receiver.responseWriterCh <- responseWriter }()
+		// If already get the path
+		if len(pi.receiverResWriterCh) != 0 {
+			resWriter.Header().Set("Access-Control-Allow-Origin", "*")
+			resWriter.WriteHeader(400)
+			resWriter.Write([]byte("[ERROR] The number of receivers has reached limits.\n"))
+			return
+		}
+		go func() { pi.receiverResWriterCh <- resWriter }()
 		// Wait for finish
-		<-receiver.sendFinishedCh
+		<-pi.sendFinishedCh
 	case "POST":
 		fallthrough
 	case "PUT":
-		receiverResWriter := <-receiver.responseWriterCh
+		receiverResWriter := <-pi.receiverResWriterCh
 		// TODO: Hard code: content-type
 		receiverResWriter.Header().Add("Content-Type", "application/octet-stream")
 		io.Copy(receiverResWriter, req.Body)
-		receiver.sendFinishedCh <- struct{}{}
-		delete(s.pathToReceiver, path)
+		pi.sendFinishedCh <- struct{}{}
+		delete(s.pathToPipe, path)
 	}
 	fmt.Printf("Trasfering %s has finished in %s method.\n", req.URL.Path, req.Method)
 }
